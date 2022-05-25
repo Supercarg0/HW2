b6c74af31a5c650b96a631db7188114d2c4ecf40
Igor Popov <popov_i_v@mail.ru>
2022-05-25 08:42:25 +0300

    add HW2-10-solution

--- HW2-10/Program.cs
+++ HW2-10/Program.cs
@@ -4,3 +4,16 @@
 782 -> 8
 918 -> 1
 */
+
+Console.Write("Enter a three-digit number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 99 && Number < 999)
+{
+    int Result = (Number % 100);
+    Result = (Result/10);
+    Console.WriteLine(Number+" -> "+Result);
+}
+else
+{
+  Console.WriteLine("Input error! A non-three-digit number is entered. ");  
+}
