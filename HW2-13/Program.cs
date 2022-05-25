ed8fef899fd619b09c3d0a4c907d40e69b63bf2e
Igor Popov <popov_i_v@mail.ru>
2022-05-25 08:54:16 +0300

    add HW2-13-solution

--- HW2-13/Program.cs
+++ HW2-13/Program.cs
@@ -5,3 +5,25 @@
 32679 -> 6
 */
 
+Console.Write("Enter a number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Result;
+if (Number > 99 && Number < 999)
+{
+    Result = (Number % 10);
+    Console.WriteLine(Number+" -> "+Result);
+}
+else if (Number > 999 && Number < 9999)
+{
+    Result = ((Number % 100)/10);
+    Console.WriteLine(Number+" -> "+Result);
+}
+else if (Number > 9999)
+{
+    Result = ((Number % 1000)/100);
+    Console.WriteLine(Number+" -> "+Result);
+}
+else
+{
+    Console.WriteLine(Number+" ->  There is no third digit!");
+}
